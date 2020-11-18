93ee2b761d586efb3b9658c2b3d0050778b9aeb7
Oliver Harrison <oliverharrison255@gmail.com>
2020-11-18 10:07:11 -0800

    added welcom text for main and index as well as added buttons for locations and rating while changing the api to write to a location

--- scripts/firebase_api.js
+++ scripts/firebase_api.js
@@ -32,36 +32,33 @@ function writeCommentData() {
   var ratingValue3 = document.getElementById("select3").value;
   var ratingValue4 = document.getElementById("select4").value;
   var ratingValue5 = document.getElementById("select5").value;
-  if (document.getElementById("select1").checked){
+  if (document.getElementById("select1").checked) {
     var ratingValue = ratingValue1;
-  }
-  else if (document.getElementById("select2").checked){
+  } else if (document.getElementById("select2").checked) {
     var ratingValue = ratingValue2;
-  }
-  else if (document.getElementById("select3").checked){
+  } else if (document.getElementById("select3").checked) {
     var ratingValue = ratingValue3;
-  }
-  else if (document.getElementById("select4").checked){
+  } else if (document.getElementById("select4").checked) {
     var ratingValue = ratingValue4;
-  }
-  else if (document.getElementById("select5").checked){
+  } else if (document.getElementById("select5").checked) {
     var ratingValue = ratingValue5;
-  }
-  else {
+  } else {
     var ratingValue = "No Rating";
   }
 
   var date = new Date();
   var datePrint = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
-  
+
   console.log("Left a rating/comment");
   const db = firebase.firestore();
-  db.collection("REV_ID").add({
-    COMMENT: inputValue,
-    RATING: ratingValue,
-    DATE: datePrint
-  });
-  
+  db.collection("REV_ID").doc("Location1").set({
+      COMMENT: inputValue,
+      RATING: ratingValue,
+      DATE: datePrint
+    }).then(function () {
+      console.log("Document successfully written!");
+    });
+
   document.getElementById('select1').checked = false;
   document.getElementById('select2').checked = false;
   document.getElementById('select3').checked = false;
